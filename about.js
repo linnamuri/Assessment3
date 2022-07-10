67b8601f80bfd1ab2b2c9c4d81765c359eaa7177
linnamuri <lakshmi.innamuri@gmail.com>
2022-07-10 01:15:43 -0400

    Part2 complete

--- about.js
+++ about.js
@@ -5,10 +5,26 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	//console.log('form submit');
+	alert("Form has been submitted successfully");
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+
+function onmouseover(evt) {
+	evt.preventDefault();
+	
+	alert("Give the user a compliment");	
+}
+
+
+let image=document.querySelector('#rubberDuck')
+//console.log(image)
+image.addEventListener('mouseover', onmouseover);
+
+
